5c220b00cf7a5d52f8c34af6029ca9612b227d83
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-02-22 22:12:21 +0000

    Исправления и улучшения
    - Улучшения в оформлении заданий;
    - Улучшения в теории;
    - Настроено автоматическое изменение версий проекта.

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 
 setup(
     name="topic_5",
-    version="1.0.1",
+    setup_requires=['bump2version'],
+    version="{major}.{minor}.{patch}".format(**{
+        "major": 1,
+        "minor": 0,
+        "patch": 1,
+    }),
     author="Shoxrux Yuldashov",
-    description="5.Основы коллекций в Python",
+    description="5. Основы коллекций в Python",
 )
